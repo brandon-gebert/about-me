a832c8349da2ee04e718dd1d3d067c8036f31b39
brandon-gebert <brandon.gebert@proton.me>
2024-12-15 23:46:38 -0500

    Updated index.html, contact.html, script.js; added GebertBrandon_Resume.pdf

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,4 @@
+// Navigation Bar Scrolling
 document.addEventListener("DOMContentLoaded", function () {
   const carousels = document.querySelectorAll(".overflow-x-scroll");
   carousels.forEach((carousel) => {
@@ -25,3 +26,22 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// Mobile Navigation Bar Toggle (Open/Close)
+const openMobileNavbarButton = document.querySelector(
+  ".open-mobile-navbar-button",
+);
+const closeMobileNavbarButton = document.querySelector(
+  ".close-mobile-navbar-button",
+);
+const mobileNavbar = document.querySelector(".mobile-navbar");
+
+if (openMobileNavbarButton && closeMobileNavbarButton && mobileNavbar) {
+  openMobileNavbarButton.addEventListener("click", () => {
+    mobileNavbar.classList.remove("hidden");
+  });
+
+  closeMobileNavbarButton.addEventListener("click", () => {
+    mobileNavbar.classList.add("hidden");
+  });
+}
